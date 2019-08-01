805105460034a755b3fda37d8ebd392ee9e707ec
Paul Uteza <utezapaul@eisti.eu>
2019-08-01 15:50:19 +0200

    Sphinx doc try #3

--- src/cicada/preprocessing/converttonwb.py
+++ src/cicada/preprocessing/converttonwb.py
@@ -1,10 +1,19 @@
-class ConvertToNWB:
+from abc import ABC
 
+
+class ConvertToNWB(ABC):
+    """NWB file object class"""
     def __init__(self, nwb_file):
+        """
+        Args:
+            nwb_file (NWB.file): NWB file object
+        """
         self.nwb_file = nwb_file
 
     def convert(self, **kwargs):
-        """
-        Convert the data and add to the nwb_file
+        """Convert the data and add to the nwb_file
+
+        Args:
+            **kwargs: arbitrary arguments
         """
         pass
