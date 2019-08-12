5499431c85629c8680ac3d68388bac54f3d5a481
Paul Uteza <utezapaul@eisti.eu>
2019-08-12 16:37:57 +0200

    Progress bar in analysis window with TQDM

--- src/cicada/analysis/cicada_psth_analysis.py
+++ src/cicada/analysis/cicada_psth_analysis.py
@@ -1,5 +1,7 @@
 from cicada.analysis.cicada_analysis import CicadaAnalysis
-
+from tqdm import tqdm
+import sys
+from time import sleep
 
 class CicadaPsthAnalysis(CicadaAnalysis):
     def __init__(self):
@@ -85,7 +87,9 @@ class CicadaPsthAnalysis(CicadaAnalysis):
         :param kwargs:
         :return:
         """
-        for data in self._data_to_analyse:
+        for data in tqdm(self._data_to_analyse, file=sys.stdout, ncols=100, desc=self.name, ascii=False):
             print(f"PSTH ----- {data.identifier} on range {kwargs['psth_range']} with {kwargs['plot_options']} "
                   f"plot using {kwargs['segmentation']} "
-                  f"with formats {kwargs['save_formats']}")
+                  f"with formats {kwargs['save_formats']} ")
+        # for i in tqdm(range(10),file=sys.stdout, ncols=100, ascii=False):
+        #     sleep(1)
